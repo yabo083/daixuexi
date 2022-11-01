0eb22645c5f4fe64469ae0588dcad7e5d333d74d
yabo083 <103875886+yabo083@users.noreply.github.com>
2022-11-01 22:43:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,10 +26,10 @@ def zip_ya(startdir):
 # 发送邮件函数
 def sendmail(path):
     host_server = 'smtp.qq.com'  # 使用qq邮箱smtp服务器
-    sender_qq = '2657455842'  # sender_qq为发件人的qq号
-    pwd = 'xbetfxykhvxvebee'  # pwd为qq邮箱的授权码
-    sender_qq_mail = '2657455842@qq.com'  # 发件人的邮箱
-    receiver = ['2657455842@qq.com', '2674180551@qq.com']  # 收件人邮箱,'2534096833@qq.com'
+    sender_qq = '#########'  # sender_qq为发件人的qq号
+    pwd = '########'  # pwd为qq邮箱的授权码
+    sender_qq_mail = '######@qq.com'  # 发件人的邮箱
+    receiver = ['#######@qq.com', '#######@qq.com']  # 收件人邮箱,'######@qq.com'
     mail_title = '今日份照片'  # 邮件标题
 
     smtp = smtplib.SMTP_SSL(host_server)  # ssl登录邮箱客户端
@@ -40,7 +40,7 @@ def sendmail(path):
     ##定义邮箱内容
     msg = MIMEMultipart()  # 生成一个邮箱对象
     msg["Subject"] = Header(mail_title, 'utf-8')  # 添加邮箱主题
-    msg["From"] = "2657455842@qq.com"  # 添加邮箱发送方
+    msg["From"] = "##########@qq.com"  # 添加邮箱发送方
     msg["To"] = ";".join(receiver)  # 添加邮箱接收方
 
     ##添加正文
@@ -69,64 +69,3 @@ if __name__ == '__main__':
     else:
         print('no such file:%s' % "photos.zip")
 
-# # 创建文件夹函数
-# def mkdir(path):
-#     folder = os.path.exists(path)
-#     if not folder:
-#         os.makedirs(path)
-#         print("创建文件夹成功")
-#     else:
-#         print("文件夹已存在")
-
-
-# 获取今天明天昨天的日期
-# today = datetime.date.today().strftime("%Y%m%d")
-# tomorrow = (datetime.date.today() + datetime.timedelta(days=1)).strftime("%Y%m%d")
-# yesterday = (datetime.date.today() + datetime.timedelta(days=-1)).strftime("%Y%m%d")
-
-# 定义文件路径
-
-
-# # 定义昨天的压缩文件
-# yesterdayfile = path + yesterday + '.zip'
-# uyesterdayfile = yesterdayfile
-
-# # 计算今天文件夹下的文件个数
-# filenum = 0
-# for filename in os.listdir(utodaypath):
-#     filenum += 1
-
-# 创建明天的文件夹
-# mkdir(utomorrowpath)
-
-# 删除昨天的压缩文件
-# if os.path.exists(uyesterdayfile):  # 如果文件存在
-#     os.remove(uyesterdayfile)
-# else:
-#     print('no such file:%s' % uyesterdayfile)
-
-# # 获取今天是周几
-# weekoftoday = datetime.date.today().weekday()
-# # 节假日列表
-# holiday = ['20180924', '20181001', '20181002', '20181003', '20181004', '20181005']
-# # 补班列表
-# workday = ['20180924', '20180925']
-#
-# # 是否是周末
-# isweekend = (weekoftoday == 5 or weekoftoday == 6)
-# # 是否是小长假
-# isholiday = today in holiday
-# # 是否不要补班
-# isworkday = today not in workday
-# # 文件夹是否为空
-# isnullfile = (filenum == 0)
-#
-# # 判断是否要压缩文件并发送邮件
-# # 周末、工作日放假的节假日、文件夹为空时不执行
-# # 补班的周末例外
-# if isnullfile:
-#     pass
-# else:
-#     if ((isweekend or isholiday) and isworkday):
-#         pass
-#     else:
